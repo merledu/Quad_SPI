7d98fb3dceca0471eab62ffc638cc3b7bd9a0945
Muhd-Waleed <waleed.waseem99@gmail.com>
2022-06-02 23:41:34 +0500

    added rx_shift

---
+++ RTL/qspi_rx_shift.sv
@@ -0,0 +1,32 @@
+module qspi_rx_shift(
+  input  logic       clk_i,
+  input  logic       rst_ni,
+  input  logic [3:0] qsd_i,
+  input  logic       lsb,
+  input  logic       msb,
+  input  logic       valid,
+  output logic [31:0] data_o
+
+);
+
+  logic [31:0] data_reg;
+
+  always @(posedge clk_i or negedge rst_ni) begin
+    if(!rst_ni) begin
+      data_reg <= '0;
+    end
+    else if (valid) begin
+      if(lsb) begin
+        data_reg  <= {data_reg[27:0], index};
+      end
+      else if (msb) begin
+        data_reg <= {index, data_reg[27:0]};
+      end
+    end
+  end
+
+  assign data_o <= data_reg;
+
+
+
+endmodule
---
+++ RTL/qspi_rx_tb_top.v
@@ -0,0 +1,32 @@
+module qspi_rx_tb_top();
+  logic        inst_clk_i;
+  logic        inst_rst_ni;
+  logic [3:0]  inst_qsd_i;
+  logic        inst_lsb;
+  logic        inst_msb;
+  logic        inst_valid;
+  logic [31:0] inst_data_o;
+
+ qspi_rx_shift u_qspi_rx_shift(
+  .clk_i(inst_clk_i),
+  .rst_ni(inst_rst_ni),
+  .qsd_i(inst_qsd_i),
+  .lsb(inst_lsb),
+  .msb(inst_msb),
+  .valid(inst_valid),
+  .data_o(inst_data_o)
+);
+
+  initial begin
+        inst_clk_i = 0; inst_rst_ni = 0;
+    #10 inst_rst_ni = 1 ; valid = 1; lsb = 1; inst_qsd_i = 4'd1;
+    #10 inst_qsd_i = 4'd2;
+    #10 inst_qsd_i = 4'd3;
+    #10 inst_qsd_i = 4'd4;
+    #10 inst_qsd_i = 4'd5;
+    #10 inst_qsd_i = 4'd6;
+    #10 inst_qsd_i = 4'd7;
+    #10 inst_qsd_i = 4'd8;
+  end
+
+endmodule
